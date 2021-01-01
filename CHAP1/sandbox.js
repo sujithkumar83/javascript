454bb0aba48a957b910d4132958a30d3dff9797f
Sujith Kumar <sujith.kumar83@outlook.com>
2021-01-01 22:06:11 +0530

    first commit

---
+++ CHAP1/sandbox.js
@@ -0,0 +1,11 @@
+//Object Literals
+
+let user = {
+    name: 'sk',
+    age: 37,
+    email: 'sujith.kumar83@outlook.com',
+    location:'Gurgaon'
+};
+
+console.log(user.name);
+console.log(user['name']);
